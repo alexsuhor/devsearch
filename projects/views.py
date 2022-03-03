d57f562299333923ba5c2c61961b3df85fae02cf
Alexsuhor <alsuhmail@gmail.com>
2022-03-03 20:19:35 +0500

    models

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+
+projectList = [
+    {
+        'id': '1',
+        'title': 'Ecommerse website',
+        'description': 'Fully functional ecommerse website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio website',
+        'description': 'This was a project where I built out my portfolio'
+    },
+    {
+        'id': '3',
+        'title': 'Social network',
+        'description': 'Awesome open source project I am still working'
+    },
+]
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    proj = Project.objects.all()
+    params = {'projects': proj}
+    return render(request, 'projects/projects.html', params)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html', {'project': projectObj})
